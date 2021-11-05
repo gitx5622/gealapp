257d4e8e969903dac75165258ef9c32477f2a29a
gitx5622 <ggitau5622@gmail.com>
2021-11-05 13:18:57 +0300

    imporoved system

--- pages/dashboard/maps/servicemen.js
+++ pages/dashboard/maps/servicemen.js
@@ -1,3 +1,26 @@
+import React, {useEffect} from 'react';
+import GealDashboard from '../../../components/dashboard/geal-dashboard';
+
+
+const ServicemenMaps = () => {
+
+    useEffect(() => {
+        try {
+            JSON.parse(localStorage.currentUser);
+            JSON.parse(localStorage.roles);
+        } catch (error) {
+            localStorage.clear();
+            window.location.replace('/');
+        }
+    }, []);
+
+    return (
+        <GealDashboard page='dashboard' section='servicemen-maps'/>
+    );
+};
+
+export default ServicemenMaps;
+
 // import React, {useEffect, useState} from 'react'
 //
 // const Servicemen = () => {
