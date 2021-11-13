ab8e3f6542094bd1482576e9ab1de49c4f71f13f
gitx5622 <ggitau5622@gmail.com>
2021-11-13 14:19:34 +0300

    homepage quickfix

--- pages/index.js
+++ pages/index.js
@@ -43,10 +43,6 @@ const Index = () => {
                 <meta name="viewport" content="initial-scale=1, width=device-width" />
                 <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
                 <link rel="icon" href="/favicon.ico" />
-                <link rel="stylesheet" type="text/css" charSet="UTF-8"
-                      href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"/>
-                <link rel="stylesheet" type="text/css"
-                      href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"/>
             </Head>
             <NavBarInstance activeKey={activeKey} onSelect={setActiveKey} />
             <Box sx={styles.navbar}>
