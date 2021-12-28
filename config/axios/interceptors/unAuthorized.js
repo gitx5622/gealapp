b5c9e411d8120a85d7860d35502543f8a3038ecf
gitx5622 <ggitau5622@gmail.com>
2021-12-28 10:28:07 +0300

    solved invalid token error

--- config/axios/interceptors/unAuthorized.js
+++ config/axios/interceptors/unAuthorized.js
@@ -2,13 +2,13 @@ export default function unAuthorizedInterceptor(axiosConfig) {
   axiosConfig.interceptors.response.use(
     response => response,
     error => {
-      if (error.response && error.response.status === 406) {
+      if (error.response && error.response.status === 401) {
         let currentUser = localStorage.currentUser;
         localStorage.clear();
         if (currentUser) {
           localStorage.currentUser = currentUser;
         }
-        window.location.replace('/');
+        window.location.replace('/user/login');
       }
 
       return Promise.reject(error);
